475eb48fa12d64e86bb4f497b8403626197a5354
Carl-Friedrich Luebeck <kafi@mac.com>
2018-05-04 14:22:28 +0200

    Adding some optical spice (preliminary version) to the output.

--- main.py
+++ main.py
@@ -6,18 +6,31 @@ import fuzzy
 import string
 
 def clearScreen():
-    print(chr(27) + "[2J" + chr(27) + "[H")
+    print(chr(27) + "[2J" + chr(27) + "[H", end='')
 
+def resetColor():
+    print(chr(27) + "[0m", end='')
+
+def grayScale(level):
+    cmap = ["37;1", "37"]
+    if level > 1:
+        level = 1
+    resetColor()
+    print(chr(27) + "[" + str(cmap[level]) + ";40m", end='')
+
+def highlight():
+    print(chr(27) + "[37;1;4m", end='')
+    
 if True:
     pw = getpass.getpass()
     kp = PyKeePass('/home/kafi/passworddb.kdbx', password=pw)
-
 
 reqstring = ''
 currentFavs = ()
 
 clearScreen()
 
+print("How to: Ctrl-C, Ctrl-D to abort, Enter to print password")
 print("Input your fuzz:")
 
 while True:
@@ -28,18 +41,36 @@ while True:
         bestMatch = currentFavs[0][1]
         print("Password for '", bestMatch.title, "':\t", bestMatch.password)
         break
-    elif char == '\x7f' and reqstring:
-        reqstring = reqstring[:-1]
+    elif char == '\x7f':
+        if (reqstring):
+            reqstring = reqstring[:-1]
     elif not char in string.printable:
         print("Unhandled unprintable char: ", repr(char))
     else:
         reqstring += char
     
-    scores = [e for e in [(fuzzy.fuzzyScore(reqstring, str(entry)), entry) for entry in kp.entries] if e[0] != None]
+    scores = [e for e in [(fuzzy.fuzzyScore(reqstring, str(entry)), entry) for entry in kp.entries]
+                if e[0] != None]
     sortedScores = sorted(scores, key=lambda e: e[0], reverse=True)
     clearScreen()
-    print("Input your fuzz:")
-    if reqstring : print("SearchString: " + reqstring)
+    print("How to: Ctrl-C, Ctrl-D to abort, Enter to print password")
+    print("Input your fuzz: ", end='')
+    if reqstring :
+        highlight()
+        print(reqstring)
+        resetColor()
+    else:
+        print('')
+
     currentFavs = sortedScores[:3]
+    i = 0
     for e in currentFavs:
-            print(e[1])
+        # note: output may currently be 'mangled' to due a bug in pykeepass
+        if len(currentFavs) == 1:
+            highlight()
+        else:
+            grayScale(i)
+        i = i+1
+        print(e[1])
+    resetColor()
+resetColor()
